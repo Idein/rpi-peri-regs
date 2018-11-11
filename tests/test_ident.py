fe524a69e4ccab97d146c5ad1a004fc7b054d112
Sugizaki Yukimasa <ysugi@idein.jp>
2018-11-11 19:24:15 +0900

    tests/test_ident: Test for IDENT values not to be the same
    It is reported that we cannot access V3D with vld/vst (NEON) or in
    non-32-bit unit.  If we do so, the same value is read in 16-byte unit.
    This is a test for that.

--- tests/test_ident.py
+++ tests/test_ident.py
@@ -3,8 +3,21 @@ from rpi_peri_regs.regs import *
 
 def test_ident():
     with rpi_peri_regs.peri.peri() as peri:
-        v = peri[V3D_IDENT0]
-        if v == (2<<24) | (ord('D')<<16) | (ord('3')<<8) | (ord('V')<<0):
+        v0 = peri[V3D_IDENT0]
+        v1 = peri[V3D_IDENT1]
+        v2 = peri[V3D_IDENT2]
+        v3 = peri[V3D_IDENT3]
+
+        if v0 == (2<<24) | (ord('D')<<16) | (ord('3')<<8) | (ord('V')<<0):
             print('V3D is enabled')
         else:
-            print('V3D is NOT enabled: 0x%08x' % v)
+            raise RuntimeError('V3D is NOT enabled: 0x%08x' % v0)
+
+        if v0 == v1:
+            raise RuntimeError('error: IDENT0 and IDENT1 is the same ' +
+                    '(maybe because accessing peripheral with vld/vst (NEON))')
+
+        print("V3D Identification 0 = 0x%08x" % v0)
+        print("V3D Identification 1 = 0x%08x" % v1)
+        print("V3D Identification 2 = 0x%08x" % v2)
+        print("V3D Identification 3 = 0x%08x" % v3)
